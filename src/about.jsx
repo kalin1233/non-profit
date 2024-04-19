ec9230f1102979f632fb7eae80ea3e07c7ada953
Aarsh Mehta <113206841+aarsh-mehta@users.noreply.github.com>
2024-04-19 13:56:49 -0400

    Update about.jsx

--- src/about.jsx
+++ src/about.jsx
@@ -1,5 +1,16 @@
 function About() {
-    return <h1>About Page</h1>;
+    return (
+        <div>
+            <h1>About Us</h1>
+            <h2>Meet the Team</h2>
+            <ul>
+                <li>💙 Ashley Anyanwu - CO 2025</li>
+                <li>🤷‍♂️ Kalin Toussaint - CO 2024</li>
+                <li>🤖 Aarsh Mehta - CO 2025</li>
+                <li>🫡 Thang Tong - CO 2025</li>
+            </ul>
+        </div>
+    );
 }
 
 export default About;
