7b6f3d12af52af68076d3d2467409db767566b6c
ashleys44 <103696910+ashleys44@users.noreply.github.com>
2024-04-19 14:43:06 -0400

    Update mission.jsx
    Added mission statement blurb

--- src/mission.jsx
+++ src/mission.jsx
@@ -1,5 +1,16 @@
 function Mission() {
-    return <h1>Mission Statement Page</h1>;
+    return <h1>Mission Statement Page</h1>
+        <div>
+        <p>
+            Empowering the Next Generation: Transforming Lives in Our Low-Income Community
+        </p>
+        <p>
+            As founders of Make It Out(MIO), we are passionately committed to creating life-changing opportunities for the youth of our community. In a landscape where low-income families often face formidable challenges, we believe in providing more than just assistance. We offer pathways to empowerment and success.
+            Our non-profit organization is a beacon of hope for those who seek change, guidance, and support. Through mentorship programs, educational resources, and financial assistance, we equip young individuals with the necessary tools to thrive and excel. By addressing the root causes of inequality and limited access to resources, we strive to break the cycle of poverty and build a brighter future for generations to come.
+            We understand community involvment to be an important element in affecting meaningful change. That is why we invite everyone to join us in our cause. Together we can create a community where every young person has the opportunity to fulfill their potential and contribute positively to society.
+            Join us in empowering our youth, transforming lives, and building a stronger, more vibrant community for all.
+        </p>
+        ;
 }
 
 export default Mission;
