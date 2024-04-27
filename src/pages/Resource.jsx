2243e503c5a689afaa78c282c09df695125455a7
Tat Thang Tong <tongtt@bu.edu>
2024-04-26 21:14:16 -0400

    Updated Resource.jsx
    added first content

--- src/pages/Resource.jsx
+++ src/pages/Resource.jsx
@@ -1,5 +1,64 @@
+import styled from 'styled-components';
+
+const PageTitle = styled.h2`
+    font-size: 24px;
+    margin-bottom: 10px;
+`
+const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    font-size: 20px;
+`;
+
+const ContentContainer = styled.div`
+    width: 80%;
+    padding-left: 20px;
+    text-align: left;
+`
+
+const StyledUl = styled.ul`
+    font-weight: bold;
+    font-size: 16px;
+    margin-bottom: 10px;
+`
+
+const StyledLi = styled.li`
+    font-weight: normal;
+    font-size: 16px;
+    margin-bottom: 10px;
+`
+
 function Resource() {
-    return <h1>Resource Page</h1>;
+    return (
+        <PageContainer>
+            <PageTitle>Resource Page</PageTitle>
+            <ContentContainer>
+                <StyledUl>Financial Learning:
+                    <StyledLi>
+                        <a href={"https://www.khanacademy.org/college-careers-more/financial-literacy"}>Khan Academy</a>
+                    </StyledLi>
+                    <StyledLi>
+                        <a href={"https://www.skillshare.com/en/browse/lifestyle"}>Skill Share</a>
+                    </StyledLi>
+                </StyledUl>
+                <StyledUl>Education:
+                    <StyledLi>
+                        <a href={"https://www.youtube.com/channel/UCtFRv9O2AHqOZjjynzrv-xg"}>Youtube Learning</a>
+                    </StyledLi>
+                    <StyledLi>
+                        <a href={"https://www.ed.gov/coronavirus/resources-for-learning-at-home"}>U.S. Department of Education</a>
+                    </StyledLi>
+                </StyledUl>
+                <StyledUl>Dummy Donation page for those who want to help:
+                    <StyledLi>
+                        <a href={"https://form.jotform.com/241088150592153"}>Dummy Donation page</a>
+                    </StyledLi>
+                </StyledUl>
+            </ContentContainer>
+        </PageContainer>
+    )
 }
 
 export default Resource;
