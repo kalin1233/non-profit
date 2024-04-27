7fc7ca85ee367e7ba1fe0343dd78fc4d70ff5794
ashleys44 <103696910+ashleys44@users.noreply.github.com>
2024-04-26 20:45:14 -0400

    Update Mission.jsx

--- src/pages/Mission.jsx
+++ src/pages/Mission.jsx
@@ -3,31 +3,47 @@ import styled from 'styled-components';
 import missionphoto from '../assets/mission.jpeg';
 import { WindupChildren, Pace} from "windups";
 
+
+const PageContainer = styled.div`
+    background-color: black;
+    color: white;
+    min-height: 100vh;
+    padding: 0 20px;
+    `;
+
 const StyledImg = styled.img`
-    width: 90%;
-    max-height: 10%;
+    width: 100%;
+    height: 37.5vh;
+    display: block;
+    margin-top: 20px;
 `; 
 
 const Boldheader = styled.h1`
     font-weight: bold;
     text-align: center;
     font-size: 24px;
-    color:black;
     width: 100%;
     margin-top:50px;
+    margin-bottom: 0;
 `;
 
 const ParagraphContainer = styled.div`
     width: 80%;
-    margin: 20vh auto;
+    margin: 0 auto 20vh;
     padding-lef: 20px;
     text-align: left
+`;
+
+const Boldparagraph = styled.p`
+    font-weight: bold;
 `;
 
 const StyledFooter = styled.footer`
     text-align: center;
     padding: 10px 0;
     position: relative;
+    bottom: 0;
+    width: 100%
 `;
 
 
@@ -41,9 +57,9 @@ function Mission() {
                 </WindupChildren>
             </Boldheader>
             <ParagraphContainer>
-                <p>
+                <BoldParagraph>
                     Empowering the Next Generation: Transforming Lives in Our Low-Income Community
-                </p>
+                </BoldParagraph>
                 <p>
                     As founders of Make It Out(MIO), we are passionately committed to creating life-changing opportunities for the youth of our community. In a landscape where low-income families often face formidable challenges, we believe in providing more than just assistance. We offer pathways to empowerment and success.
                     Our non-profit organization is a beacon of hope for those who seek change, guidance, and support. Through mentorship programs, educational resources, and financial assistance, we equip young individuals with the necessary tools to thrive and excel. By addressing the root causes of inequality and limited access to resources, we strive to break the cycle of poverty and build a brighter future for generations to come.
