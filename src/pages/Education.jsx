3c49affd477f0e4af01cbf770382b3129bb10dbd
Favor Wariboko <66702423+FavorWa@users.noreply.github.com>
2024-04-28 23:24:36 -0400

    last changes for education

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,57 +1,63 @@
 import educationPic from '../assets/education.png';
 import styled, {ThemeProvider} from 'styled-components';
 import {theme} from "../components/Theme.jsx";
+import { WindupChildren } from 'windups';
 
 const StyledImg = styled.img`
-    width: 100%;
-    max-height: 100%;
-    position: absolute;
+    height: 417px;
+    width: 626px;
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 20px;
 `;
 
-const OverlayDiv = styled.div`
-    position: absolute;
-    background-color: rgba(0, 0, 0, 0.5);
-    top: 30px;
-    left: 0;
-    width: 100%;
-    height: 100%;
-`;
 
 const StyledHeader = styled.h1`
-    position: absolute;
-    color: white;
+    text-align: center;
+    color: black;
     font-size: ${({ theme }) => theme.fontSizes.pageTitle};
     top: 0;
 `;
 
 const StyledP = styled.p`
-    position: absolute;
+    text-align: center;
     font-size: ${({theme}) => theme.fontSizes.medium};
-    color: white;
-    top: 150px;
+    color: black;
     width: 80%;
+    margin: 0 auto;
+    
+    
 `;
 
 const Styleda = styled.a`
-color: lightblue;
-text-decoration: underline;
-cursor: pointer;
+    color: blue;
+    text-decoration: underline;
+    cursor: pointer;
 `;
 
 export default function Education() {
     return (
         <ThemeProvider theme={theme}>
         <div>
+        <StyledHeader>
+        <WindupChildren>
+                        <pace ms={100}>{"Education"} </pace>
+                    </WindupChildren>
+        </StyledHeader>
              <StyledImg src={educationPic} alt="Education"/>
-             <OverlayDiv/>
-             <StyledHeader>Education</StyledHeader>
+             
+             
              <StyledP>Quality education can be scarce in low-income communities. Many youth lack tutoring, mentorship, and essential resources, hindering their potential. Our non-profit breaks these barriers, providing free support and resources for every young person to thrive academically.
 Our programs empower youth with real-world knowledge, skills, and experiences. From tutoring to financial literacy education, we bridge the gap between aspiration and achievement.
 We understand the challenges faced by families, which is why our services are free. Our goal is to ensure every child, regardless of background, has the tools to succeed.
 While the <Styleda href="https://mysticlearningcenter.org/about-us" target="_blank" rel="noopener noreferrer" >Mystic Learning Center</Styleda> is a vital resource, we aim to expand opportunities for growth. Join us in empowering youth and building a stronger community through education.
 </StyledP>       
         </div>
+        <footer>
+                © {new Date().getFullYear()} Non-Profit. All rights reserved.
+            </footer>
         </ThemeProvider>
+        
 
     );
 }
